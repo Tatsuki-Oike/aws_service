e4ee01532784e867874680bbb6225fe5bfe22eba
Tatsuki-Oike <yy5433268@gmail.com>
2023-09-22 16:49:31 +0900

    first commit

--- 05_S3/4_url_download.py
+++ 05_S3/4_url_download.py
@@ -22,7 +22,7 @@ presigned_url = s3.generate_presigned_url(
     ExpiresIn=60  # 有効期限（秒単位）を設定
 )
 
-# Presigned URLでデータ取得
+# Presigned URLでファイルのダウンロード
 response = requests.get(presigned_url)
 
 if response.status_code == 200:
